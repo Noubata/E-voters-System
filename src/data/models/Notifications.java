ad97c50a180ea9630c7f34fa90ca30944201fbd2
Beny <benynouba@gmail.com>
2025-12-31 11:48:50 +0100

    updated repositories

--- src/data/models/Notifications.java
+++ src/data/models/Notifications.java
@@ -1,4 +1,22 @@
 package data.models;
 
 public class Notifications {
+    private String phonenUmber;
+    private String email;
+
+    public String getPhonenUmber() {
+        return phonenUmber;
+    }
+
+    public void setPhonenUmber(String phonenUmber) {
+        this.phonenUmber = phonenUmber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
