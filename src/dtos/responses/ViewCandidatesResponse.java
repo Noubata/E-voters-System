c2e8bc81f269fb0bebffd0972ec29c1a20b8ab1f
Beny <benynouba@gmail.com>
2026-01-01 10:12:49 +0100

    voter's responses for viewing candidates

---
+++ src/dtos/responses/ViewCandidatesResponse.java
@@ -0,0 +1,31 @@
+package dtos.responses;
+
+public class ViewCandidatesResponse {
+    private String name;
+    private String party;
+    private String candidateId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParty() {
+        return party;
+    }
+
+    public void setParty(String party) {
+        this.party = party;
+    }
+
+    public String getCandidateId() {
+        return candidateId;
+    }
+
+    public void setCandidateId(String candidateId) {
+        this.candidateId = candidateId;
+    }
+}
